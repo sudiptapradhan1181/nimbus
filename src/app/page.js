934c86a33e8ac27bc319b1d9b7cf7dbe152c8557
Sudipta Pradhan <sudipta.pradhan@greyblue.in>
2025-02-25 15:44:29 +0530

    global auth logic added

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,11 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import agoraToken from "agora-token";
-import { parseCookies } from "nookies";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const cookies = parseCookies();
   const router = useRouter();
   useEffect(() => {
-    if (!cookies.token) {
-      router.replace("/auth");
-    } else {
-      router.replace("/dashboard");
-    }
-  }, [cookies, router]);
+    router.replace("/dashboard");
+  }, []);
   return null;
 }
