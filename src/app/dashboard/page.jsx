b91f620d5ba5551dbec489bac58019e948af6691
Sudipta Pradhan <sudiptapradhan1181@gmail.com>
2025-03-06 22:23:19 +0530

    build error fixed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,28 @@
 "use client";
+
+import { useState, useEffect } from "react";
 import { parseCookies } from "nookies";
 import { jwtDecode } from "jwt-decode";
 
 export default function Dashboard() {
-  const cookies = parseCookies();
-  const data = jwtDecode(cookies.accessToken);
-  console.log(data);
-  return <div>Welcome {data.name}!</div>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const cookies = parseCookies();
+    if (cookies.accessToken) {
+      try {
+        const decoded = jwtDecode(cookies.accessToken);
+        setUser(decoded);
+      } catch (error) {
+        console.error("Invalid Token:", error);
+      }
+    }
+  }, []);
+
+  // Render only when the user state is set
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>Welcome {user.name}!</div>;
 }
