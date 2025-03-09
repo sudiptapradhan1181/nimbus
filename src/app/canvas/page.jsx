a6ff60f2361b5dc632695873f8d984a129811046
Sudipta Pradhan <sudipta.pradhan@greyblue.in>
2025-03-09 19:46:21 +0530

    added canvas

---
+++ src/app/canvas/page.jsx
@@ -0,0 +1,9 @@
+import CanvasComponent from "./components/CanvasComponent";
+
+export default function CanvasPage() {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <CanvasComponent />
+    </div>
+  );
+}
